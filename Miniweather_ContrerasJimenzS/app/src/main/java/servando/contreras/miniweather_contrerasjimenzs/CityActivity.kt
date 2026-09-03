d08f7618fbacbae7a130d52bf004e3ebfd77aea0
Servando Contreras Jimenez <159857682+ser3154@users.noreply.github.com>
2026-09-02 19:00:16 -0600

    mejorado

--- Miniweather_ContrerasJimenzS/app/src/main/java/servando/contreras/miniweather_contrerasjimenzs/CityActivity.kt
+++ Miniweather_ContrerasJimenzS/app/src/main/java/servando/contreras/miniweather_contrerasjimenzs/CityActivity.kt
@@ -1,12 +1,21 @@
 package servando.contreras.miniweather_contrerasjimenzs
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import servando.contreras.miniweather_contrerasjimenzs.utilities.WeatherService
 
 class CityActivity : AppCompatActivity() {
+    var citySelected: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,40 @@ class CityActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val service: WeatherService = WeatherService(this)
+        val nextButton: Button = findViewById<Button>(R.id.btn_save_city)
+        val citySelector: Spinner = findViewById<Spinner>(R.id.city_selecter)
+
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item,
+            service.getCities())
+
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        citySelector.adapter = adapter
+
+        citySelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                p0: AdapterView<*>?,
+                p1: View?,
+                p2: Int,
+                p3: Long
+            ) {
+                citySelected = p0?.getItemAtPosition(p2).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        nextButton.setOnClickListener {
+            val  intent = Intent(this, MainActivity::class.java).putExtra("city", citySelected)
+        }
+        startActivity(intent)
+
     }
 }
---
+++ Miniweather_ContrerasJimenzS/app/src/main/java/servando/contreras/miniweather_contrerasjimenzs/domain/Weather.kt
@@ -0,0 +1,5 @@
+package servando.contreras.miniweather_contrerasjimenzs.domain
+
+import android.health.connect.datatypes.units.Temperature
+
+data class Weather(val temperature: Int, val weather: String)
